b20966912c6c6179b1c203e16f1893bf55afa276
andrewhpark25 <andrewhpark25@gmail.com>
2020-04-24 10:22:10 -0700

    MVP

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+import { useHistory} from 'react-router-dom';
 
 const initialColor = {
   color: "",
   code: { hex: "" }
 };
 
-const ColorList = ({ colors, updateColors }) => {
+const ColorList = ({ colors}) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
+  const { push } = useHistory();
 
   const editColor = color => {
     setEditing(true);
@@ -21,10 +23,26 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put(`/api/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => {
+       console.log(res.data);
+        push(`/protected`);
+      })
+      .catch(err => console.log(err));
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth()
+    .delete(`/api/colors/${color.id}`)
+    .then(response => {
+      console.log(response);
+      push(`/protected`); 
+    })
+    .catch(error => {
+      console.log("color was not deleted", error);
+    });
   };
 
   return (
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,61 @@
-import React from "react";
+import React, { useState} from "react";
+import { useHistory} from 'react-router-dom';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+
+
+const initialInput = {
+  username: "",
+  password:""
+};
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
+
+  const [credentials, setCredentials] = useState(initialInput);
+  const { push } = useHistory();
+ 
+ const handleChange = e => {
+   setCredentials({
+    ...credentials,
+    [e.target.name]: e.target.value
+  });
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    axiosWithAuth()
+    .post('/api/login', credentials)
+    .then(res => {
+      localStorage.setItem('token', JSON.stringify(res.data.payload));
+      push('/protected');
+})
+.catch(err => {console.log(err);
+  alert('Access Denied');
+  setCredentials({...initialInput});
+});
+}
+
+
+return (
+  <div>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="username"
+        value={credentials.username}
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        name="password"
+        value={credentials.password}
+        onChange={handleChange}
+      />
+      <button>Log in</button>
+    </form>
+  </div>
+);
 };
 
 export default Login;
